948f7293e1e5ee44bd050cb2a3fb69c7fbeaa29a
hafsaraza <71767326+hafsaraza@users.noreply.github.com>
2020-12-11 18:18:53 +0500

    s2Commit

---
+++ myfirstexpress/routes/index.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.send("<h1>hello</h1>");
+});
+router.get('/products', function(req, res, next) {
+  res.send("hello products");
+});
+
+module.exports = router;
